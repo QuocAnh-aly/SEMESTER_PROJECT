0859ae3cdf9cd926bc438b3314fc92b3798fb47a
PhucNguyen45 <101778920+PhucNguyen45@users.noreply.github.com>
2025-02-07 22:24:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int temp = n;
+
+while (n > 0)
+{
+    int r = n % 10;
+    sum = (sum * 10) + r;
+    n = n / 10;
+}
+if (temp == sum)
+{
+    Console.WriteLine("so doi xung");
+}
+else
+    Console.WriteLine("so khong doi xung");
